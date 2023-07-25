98aa13bae56a064fe745d9a40ae37038270befce
lucas_lsgsma <lucas.guimaraes@qintess.com>
2023-07-25 09:52:12 -0300

    indentation with 4 spaces

--- src/ast/Program.java
+++ src/ast/Program.java
@@ -9,93 +9,93 @@ import symbols.SymbolTable;
 
 public class Program {
 
-	private String filename;
-	private List<AbstractCommand> comandos;
-	private SymbolTable symbolTable;
+    private String filename;
+    private List<AbstractCommand> comandos;
+    private SymbolTable symbolTable;
 
-	public Program() {
-		this.comandos = new ArrayList<>();
-	}
+    public Program() {
+        this.comandos = new ArrayList<>();
+    }
 
-	public void generateTarget(String filename, TargetLang target) {
-		try {
-			var extension = generateExtension(target);
-			try (var fw = new FileWriter(filename + extension)) {
-				try (var pw = new PrintWriter(fw)) {
-					StringBuilder strBuilder = new StringBuilder();
-					generateHeader(target, strBuilder);
-					comandos.forEach(c -> {
-						System.out.print(c.generateCode(target));
-						strBuilder.append(c.generateCode(target));
-					});
-					generateFooter(target, strBuilder);
-					pw.println(strBuilder);
-				}
-			} catch (Exception ex) {
-				ex.printStackTrace();
-			}
-		} catch (Exception ex) {
-			ex.printStackTrace();
-		}
-	}
+    public void generateTarget(String filename, TargetLang target) {
+        try {
+            var extension = generateExtension(target);
+            try (var fw = new FileWriter(filename + extension)) {
+                try (var pw = new PrintWriter(fw)) {
+                    StringBuilder strBuilder = new StringBuilder();
+                    generateHeader(target, strBuilder);
+                    comandos.forEach(c -> {
+                        System.out.print(c.generateCode(target));
+                        strBuilder.append(c.generateCode(target));
+                    });
+                    generateFooter(target, strBuilder);
+                    pw.println(strBuilder);
+                }
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
 
-	private String  generateExtension(TargetLang target) {
-		return switch (target) {
-			case JAVA -> ".java";
-			case JS -> ".js";
-			case C -> ".c";
-		};
-	}
+    private String  generateExtension(TargetLang target) {
+        return switch (target) {
+            case JAVA -> ".java";
+            case JS -> ".js";
+            case C -> ".c";
+        };
+    }
 
-	private void generateFooter(TargetLang target, StringBuilder strBuilder) {
-		switch (target) {
-			case JAVA -> {
-				strBuilder.append("    }\n");
-				strBuilder.append("}\n");
-			}
-			case C -> strBuilder.append("}\n");
-		}
-	}
+    private void generateFooter(TargetLang target, StringBuilder strBuilder) {
+        switch (target) {
+            case JAVA -> {
+                strBuilder.append("    }\n");
+                strBuilder.append("}\n");
+            }
+            case C -> strBuilder.append("}\n");
+        }
+    }
 
-	private void generateHeader(TargetLang target, StringBuilder strBuilder) {
-		switch (target) {
-			case JAVA -> {
-				strBuilder.append("import java.util.Scanner;\n\n");
-				strBuilder.append("public class Main {\n");
-				strBuilder.append("    public static void main(String[] args) {\n");
-				strBuilder.append("        Scanner sc = new Scanner(System.in);\n");
-			}
-			case C -> {
-				strBuilder.append("#include <stdio.h>\n");
-				strBuilder.append("#include <string.h>\n\n");
-				strBuilder.append("int main() {\n");
-			}
-		}
-	}
+    private void generateHeader(TargetLang target, StringBuilder strBuilder) {
+        switch (target) {
+            case JAVA -> {
+                strBuilder.append("import java.util.Scanner;\n\n");
+                strBuilder.append("public class Main {\n");
+                strBuilder.append("    public static void main(String[] args) {\n");
+                strBuilder.append("        Scanner sc = new Scanner(System.in);\n");
+            }
+            case C -> {
+                strBuilder.append("#include <stdio.h>\n");
+                strBuilder.append("#include <string.h>\n\n");
+                strBuilder.append("int main() {\n");
+            }
+        }
+    }
 
-	public List<AbstractCommand> getComandos() {
-		return comandos;
-	}
+    public List<AbstractCommand> getComandos() {
+        return comandos;
+    }
 
-	public void setComandos(List<AbstractCommand> comandos) {
-		this.comandos = comandos;
-	}
+    public void setComandos(List<AbstractCommand> comandos) {
+        this.comandos = comandos;
+    }
 
-	public SymbolTable getSymbolTable() {
-		return symbolTable;
-	}
+    public SymbolTable getSymbolTable() {
+        return symbolTable;
+    }
 
-	public void setSymbolTable(SymbolTable symbolTable) {
-		this.symbolTable = symbolTable;
-	}
+    public void setSymbolTable(SymbolTable symbolTable) {
+        this.symbolTable = symbolTable;
+    }
 
-	public void run() {
-		RuntimeEntity runtime = new RuntimeEntity();
-		runtime.updateContent(symbolTable.getSymbols().values());
-		comandos.forEach(c -> {
-			c.run();
-			runtime.updateContent(symbolTable.getSymbols().values());
-		});
-		runtime.close();
-	}
+    public void run() {
+        RuntimeEntity runtime = new RuntimeEntity();
+        runtime.updateContent(symbolTable.getSymbols().values());
+        comandos.forEach(c -> {
+            c.run();
+            runtime.updateContent(symbolTable.getSymbols().values());
+        });
+        runtime.close();
+    }
 }
