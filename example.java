db4681db2c7b5ca841159480309a82e2e4e61b1c
lucas_lsgsma <lucas.guimaraes@qintess.com>
2023-07-25 09:40:23 -0300

    command line argument execution

---
+++ example.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a;
+        String f;
+        double e;
+        int b;
+        int c;
+        int d;
+        System.out.println("Hello World!");
+        System.out.println("Digite o valor C");
+        a = "Ola mundo aqui e a IsiLanguage";
+        e = 3.14;
+        b = 100*(2+12)/14;
+        d = (b+2)/2;
+        f = "Ola mundo aqui e a IsiLanguage";
+        c = sc.nextInt();
+        System.out.println(a);
+        System.out.println(f);
+        System.out.println(b);
+        System.out.println(e);
+        System.out.println(d);
+    }
+}
+
---
+++ example.js
@@ -0,0 +1,14 @@
+alert("Hello World!");
+alert("Digite o valor C");
+a = "Ola mundo aqui e a IsiLanguage";
+e = 3.14;
+b = 100*(2+12)/14;
+d = (b+2)/2;
+f = "Ola mundo aqui e a IsiLanguage";
+c = prompt("Type you input");
+alert(a);
+alert(f);
+alert(b);
+alert(e);
+alert(d);
+
