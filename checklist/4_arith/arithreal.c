fb16caf2083f94d1ffda4a97ea232968d4b61c26
lucas_lsgsma <lucas.guimaraes@qintess.com>
2023-07-30 17:47:09 -0300

    arith checklist

---
+++ checklist/4_arith/arithreal.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    double b;
+    b = 7.0*(5.0+15.0)/(2.0*5.0)-3.0;
+    printf("%s\n", "11");
+    printf("%lf\n", b);
+}
+
---
+++ checklist/4_arith/arithreal.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double b;
+        b = 7.0*(5.0+15.0)/(2.0*5.0)-3.0;
+        System.out.println("11");
+        System.out.println(b);
+        sc.close();
+    }
+}
+
