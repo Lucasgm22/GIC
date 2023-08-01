78347dd90387d6a2b45b7820c45c580576c3de57
lucas_lsgsma <lucas.guimaraes@qintess.com>
2023-08-01 11:56:44 -0300

    not initialized better message

--- src/exception/IsiUnassignedVariableException.java
+++ src/exception/IsiUnassignedVariableException.java
@@ -2,6 +2,6 @@ package exception;
 
 public class IsiUnassignedVariableException extends IsiSemanticException {
     public IsiUnassignedVariableException(String id, int line, int column) {
-        super("'" + id + "' does not have a initial value", line, column);
+        super("Identifier '" + id + "' does not have a initial value", line, column);
     }
 }
